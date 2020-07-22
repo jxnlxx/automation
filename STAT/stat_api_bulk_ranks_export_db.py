75e2a57fe3c2a262245cf5743fa275b3e1ca1263
jxnlxx <Jon.Lee@iprospect.com>
2020-07-22 17:04:41 +0100

    cleared whitespace

--- STAT/stat_api_bulk_ranks_export_db.py
+++ STAT/stat_api_bulk_ranks_export_db.py
@@ -30,7 +30,7 @@ print('\n'+'Welcome to the STAT API Bulk Ranks Request script!'
 
 davids_law1 = int(dt.datetime.today().strftime('%Y')) - 2
 davids_law2 = int(dt.datetime.today().strftime('%Y'))
-  
+
 print('\n'+'Which year would you like to retrieve exports for?')
 while True:
     try:
@@ -40,7 +40,7 @@ while True:
         else:
             break
     except ValueError:
-        print('Invalid response.')    
+        print('Invalid response.')
 
 if year == davids_law1:
     davids_law3 = int(dt.datetime.today().strftime('%m'))
@@ -64,8 +64,8 @@ while True:
             month = int(month)
             if month < davids_law3 or month > davids_law4:
                 raise ValueError
-      
-            try: 
+
+            try:
                 jobs_all = pd.read_csv(fr'L:\Commercial\Operations\Technical SEO\Automation\STAT\SQL\Requests\{year}_{month:02d}_bulk_ranks_all.csv')
                 print('\n'+f'Checking {year}_{month:02d}_bulk_ranks_all.csv...')
                 time.sleep(1)
@@ -143,7 +143,7 @@ jobs_all = jobs_all.set_index('Url')
 try:
     jobs_head = jobs_all[~jobs_all['Status'].isin(['Done'])]  # the tilde makes this 'is not in 'done''
     jobs_head = jobs_head.drop(columns=['Status'])
-# if 'Status' column doesn't exist, 
+# if 'Status' column doesn't exist,
 except KeyError:
     jobs_head = jobs_all
     jobs_all = jobs_all.assign(Status='').astype(str)
@@ -160,8 +160,8 @@ for index, row in jobs_head.iterrows(): # this construction allows us to go acro
     ranks_month_df = pd.DataFrame()
     site_url = index
     save_name = site_url.replace('/','_') # Remove slashes so file will save/load properly
-    save_name = save_name.replace('-','_') # Remove slashes so file will save/load properly   
-    save_name = save_name.replace('.','_') # Remove slashes so file will save/load properly   
+    save_name = save_name.replace('-','_') # Remove slashes so file will save/load properly
+    save_name = save_name.replace('.','_') # Remove slashes so file will save/load properly
     print('\n'+f'Checking for existing data for {site_url}...')
 
 # =============================================================================
@@ -170,7 +170,7 @@ for index, row in jobs_head.iterrows(): # this construction allows us to go acro
 
 
 
-      
+
 # =============================================================================
 # Continue with iteration through bulk_ranks df to get the keyword data
 # =============================================================================
@@ -208,58 +208,59 @@ for index, row in jobs_head.iterrows(): # this construction allows us to go acro
 
         total_kws = len(kw_list)
         print(f'Processing {total_kws} keywords...')
-        
+
         ranks_day_df = json_normalize(kw_list)
 
         # adjust column label names
         ranks_day_df.columns = ranks_day_df.columns.str.replace('Google.','Google')
         ranks_day_df.columns = ranks_day_df.columns.str.split('.').str[-1]
-        ranks_day_df = ranks_day_df.rename(columns={'Id':'KeywordId', 'date':'Date', 'type':'Type'})        
-                
+        ranks_day_df = ranks_day_df.rename(columns={'Id':'KeywordId', 'date':'Date', 'type':'Type'})
+
         # create date_id from ranks_day_df[['Date', 'Id']] and insert at index [0]
         date_id = ranks_day_df[['Date', 'KeywordId']].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
         ranks_day_df.insert(0, 'Id', date_id)
-        
+
         try:
             ranks_day_df = ranks_day_df.drop(columns=['LocalSearchTrendsByMonth'])
         except KeyError:
             pass
-        
+
         print('Processing complete!')
 
-#        ranks_df.to_csv(fr'L:\Commercial\Operations\Technical SEO\Automation\STAT\test.csv', index=False)
+        #ranks_df.to_csv(fr'L:\Commercial\Operations\Technical SEO\Automation\STAT\test.csv', index=False)
 
 
 # =============================================================================
 
         print(f'Replacing not ranking with {not_ranking}...') # configured at the start under SETTINGS
-        
+
         # replace 'None' with not_ranking in ['GoogleRank','GoogleBaseRank']
         ranks_day_df[['GoogleRank','GoogleBaseRank']] = ranks_day_df[['GoogleRank','GoogleBaseRank']].replace(to_replace=[None], value=not_ranking)
-        
+
         print('Done!')
 
         ranks_month_df = ranks_month_df.append(ranks_day_df)
 #        ranks_df.to_csv(fr'L:\Commercial\Operations\Technical SEO\Automation\STAT\test_edit.csv', index=False)
 
-#    these two breaks can be used together to test 1st day of month for 1 site without saving 
+#    these two breaks can be used together to test 1st day of month for 1 site without saving
 #        break # for testing 1st day of month (this doesn't stop it saving)
 #    break # for testing 1 client in list WITHOUT SAVING
 
 # =============================================================================
-# append full month to database     
+# append full month to database
 # =============================================================================
-    
+
     print('Saving {} to database'.format(site_url))
 
     conn = sqlite3.connect(r'L:\Commercial\Operations\Technical SEO\Automation\Data\STAT\SQL\stat_ranks_relational.db')
 
-    with conn: 
-    
+    with conn:
+
         c = conn.cursor()
-        
+
+        ##TODO: figure out a better way of doing this to avoid sql injection
         c.execute(
-            'CREATE TABLE IF NOT EXISTS ' + save_name +  ''' (
+            'CREATE TABLE IF NOT EXISTS ' + save_name +  '''
             Id TEXT PRIMARY KEY,
             KeywordId INTEGER,
             Keyword TEXT,
@@ -293,7 +294,7 @@ for index, row in jobs_head.iterrows(): # this construction allows us to go acro
             GoogleUrl TEXT
             )'''
         )
-          
+
         ranks_month_df.to_sql('{}'.format(save_name), conn, if_exists='append', schema='online', index=False)
 
         print('Done!')
@@ -307,16 +308,16 @@ for index, row in jobs_head.iterrows(): # this construction allows us to go acro
     print('\n'+f'Updating {year}_{month:02d}_bulk_ranks_all.csv...')
 
     # Update jobs_all and save
-    jobs_all.at[f'{site_url}','Status'] = 'Done' 
+    jobs_all.at[f'{site_url}','Status'] = 'Done'
     jobs_all = jobs_all.reset_index()  # reset index so it has a label when saved (problems using file in future otherwise...)
-    jobs_all.to_csv(fr'L:\Commercial\Operations\Technical SEO\Automation\STAT\SQL\Requests\{year}_{month:02d}_bulk_ranks_all.csv', index=False)        
+    jobs_all.to_csv(fr'L:\Commercial\Operations\Technical SEO\Automation\STAT\SQL\Requests\{year}_{month:02d}_bulk_ranks_all.csv', index=False)
     jobs_all = jobs_all.set_index('Url') # set index so next iteration works
     s += 1
 
     print('\n'+f'Done!')
-    
+
 #    break # for testing one client in script with saving AFTER updating bulk_ranks_all
-    
+
 # =============================================================================
 # END TIMER
 # =============================================================================
